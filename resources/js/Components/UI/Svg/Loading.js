f8a8773c8984992aeb58ed05c5e4f2f24590c96a
lilic7 <zubcolilian@mail.ru>
2024-01-04 09:19:51 +0200

    Added Playlist Components

---
+++ resources/js/Components/UI/Svg/Loading.js
@@ -0,0 +1,25 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var react_1 = require("react");
+var Loading = function (_a) {
+    var _b = _a.color, color = _b === void 0 ? "#009ef7" : _b;
+    return (react_1.default.createElement("svg", { xmlns: "http://www.w3.org/2000/svg", className: "m-auto bg-none block w-10 h-10", style: { shapeRendering: 'auto' }, viewBox: "0 0 100 100", preserveAspectRatio: "xMidYMid" },
+        react_1.default.createElement("rect", { x: "19", y: "19", width: "20", height: "20", fill: color },
+            react_1.default.createElement("animate", { attributeName: "fill", values: "#ffffff;".concat(color, ";").concat(color), keyTimes: "0;0.125;1", dur: "0.53475935828877s", repeatCount: "indefinite", begin: "0s", calcMode: "discrete" })),
+        react_1.default.createElement("rect", { x: "40", y: "19", width: "20", height: "20", fill: color },
+            react_1.default.createElement("animate", { attributeName: "fill", values: "#ffffff;".concat(color, ";").concat(color), keyTimes: "0;0.125;1", dur: "0.53475935828877s", repeatCount: "indefinite", begin: "0.06684491978609625s", calcMode: "discrete" })),
+        react_1.default.createElement("rect", { x: "61", y: "19", width: "20", height: "20", fill: color },
+            react_1.default.createElement("animate", { attributeName: "fill", values: "#ffffff;".concat(color, ";").concat(color), keyTimes: "0;0.125;1", dur: "0.53475935828877s", repeatCount: "indefinite", begin: "0.1336898395721925s", calcMode: "discrete" })),
+        react_1.default.createElement("rect", { x: "19", y: "40", width: "20", height: "20", fill: color },
+            react_1.default.createElement("animate", { attributeName: "fill", values: "#ffffff;".concat(color, ";").concat(color), keyTimes: "0;0.125;1", dur: "0.53475935828877s", repeatCount: "indefinite", begin: "0.4679144385026738s", calcMode: "discrete" })),
+        react_1.default.createElement("rect", { x: "61", y: "40", width: "20", height: "20", fill: color },
+            react_1.default.createElement("animate", { attributeName: "fill", values: "#ffffff;".concat(color, ";").concat(color), keyTimes: "0;0.125;1", dur: "0.53475935828877s", repeatCount: "indefinite", begin: "0.20053475935828877s", calcMode: "discrete" })),
+        react_1.default.createElement("rect", { x: "19", y: "61", width: "20", height: "20", fill: color },
+            react_1.default.createElement("animate", { attributeName: "fill", values: "#ffffff;".concat(color, ";").concat(color), keyTimes: "0;0.125;1", dur: "0.53475935828877s", repeatCount: "indefinite", begin: "0.40106951871657753s", calcMode: "discrete" })),
+        react_1.default.createElement("rect", { x: "40", y: "61", width: "20", height: "20", fill: color },
+            react_1.default.createElement("animate", { attributeName: "fill", values: "#ffffff;".concat(color, ";").concat(color), keyTimes: "0;0.125;1", dur: "0.53475935828877s", repeatCount: "indefinite", begin: "0.3342245989304813s", calcMode: "discrete" })),
+        react_1.default.createElement("rect", { x: "61", y: "61", width: "20", height: "20", fill: color },
+            react_1.default.createElement("animate", { attributeName: "fill", values: "#ffffff;".concat(color, ";").concat(color), keyTimes: "0;0.125;1", dur: "0.53475935828877s", repeatCount: "indefinite", begin: "0.267379679144385s", calcMode: "discrete" }))));
+};
+exports.default = Loading;
+//# sourceMappingURL=Loading.js.map
